b69f38f540d7efa46b8f0398bb8403f03c11170e
Cedric-Arizobal <ced.arizobal@gmail.com>
2025-11-04 10:45:54 +0800

    Library double split

--- src/pages/Document/MyDocuments.tsx
+++ src/pages/Document/MyDocuments.tsx
@@ -424,7 +424,7 @@ const MyDocuments: React.FC = () => {
 
       {/* Documents Grid */}
       {filteredDocs.length > 0 ? (
-        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-4">
           {documentCards}
         </div>
       ) : error ? (
